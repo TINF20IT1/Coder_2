969416fdd6edd40594a994ef23c922a674fe88d6
Botzum, Pascal <passbook@gmx.net>
2022-06-02 12:28:53 +0200

    Working on Player movement

--- Assets/Interactable.cs
+++ Assets/Interactable.cs
@@ -7,18 +7,22 @@ public class Interactable : MonoBehaviour
 {
     Collider2D boxCollider;
     SpriteRenderer spriteRenderer;
+    public PlayerController ball;
 
     // Start is called before the first frame update
     void Start()
     {
         boxCollider = GetComponent<BoxCollider2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
+        ball = FindObjectOfType<PlayerController>();
+        Debug.Log(ball.GetType());
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log($"enter {collision.name}");
         spriteRenderer.color = Color.blue;
+        //ball.movementSpeed += 3f;
     }
 
     private void OnTriggerExit2D(Collider2D collision)
--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -5,22 +5,54 @@ using System.Collections.Generic;
 [RequireComponent (typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour {
 
-    public float movementSpeed = 5.0f;
+    float horMovement, vertMovement;
+    Vector2 curVelocity;
+
+    public float movementSpeed = 3f;
+    public float jumpForce = 300f;
     private Rigidbody2D rb;
+    float distToGround;
+
+    bool isJumping, alreadyJumped;
 
     void Start () {
-        
-    }
-    
-    void Update () {
+        var collider = GetComponent<Collider2D>();
         rb = GetComponent<Rigidbody2D>();
+        distToGround = collider.bounds.extents.y;
+    }
 
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+    bool isGrounded() => Physics2D.Raycast(transform.position, -Vector2.up, distToGround + 0.1f);
 
-        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+    void Update () {
+        horMovement = Input.GetAxis("Horizontal");
+        vertMovement = Input.GetAxis("Vertical");
+        curVelocity = rb.velocity;
 
-        rb.AddForce(movement * movementSpeed);
+        //var upPressed = Input.GetKey(KeyCode.UpArrow);
+        //var spacePressed = Input.GetKey(KeyCode.Space);
 
+        //Debug.Log($"{upPressed} {spacePressed} {isGrounded()}");
+
+        //var mov = new Vector2(0, 0);
+
+        //if ((upPressed || spacePressed) && isGrounded())
+        //{
+        //    Debug.Log("Jump");
+        //    mov.y = jumpSpeed;
+        //}
+
+        //mov.x = movementSpeed * Input.GetAxis("Horizontal");
+
+
+        //Debug.Log(mov);
+        //rb.AddForce(mov);
+    }
+
+    void FixedUpdate()
+    {
+        if (horMovement != 0)
+        {
+            rb.velocity = new Vector2(horMovement * movementSpeed, curVelocity.y);
+        }    
     }
 }
