fc6f69f4d6ab51bd6592b28ea5381147a7b5c6fa
Pascal Botzum <p.botzum@gmail.com>
2022-06-15 00:31:47 +0200

    Animation support started

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -5,46 +5,53 @@ using System.Collections.Generic;
 [RequireComponent (typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour {
 
-    float horMovement, vertMovement;
-    Vector2 curVelocity;
-
     public float movementSpeed = 3f;
     public float jumpForce = 300f;
+
+    public PlayerManager.MobileDevice device;    
+
+    float horMovement, vertMovement;
+    Vector2 curVelocity;
     private Rigidbody2D rb;
     float distToGround;
 
     bool isJumping, alreadyJumped;
 
+    
     void Start () {
         var collider = GetComponent<Collider2D>();
         rb = GetComponent<Rigidbody2D>();
         distToGround = collider.bounds.extents.y;
+        Log($"{device.Id}");
     }
+
+    void Log(string msg) => Debug.Log($"#{device.Id}# {msg}");
+
 
     bool isGrounded() => Physics2D.Raycast(transform.position, -Vector2.up, distToGround + 0.1f);
 
     void Update () {
         horMovement = Input.GetAxis("Horizontal");
-        vertMovement = Input.GetAxis("Vertical");
+        vertMovement = Input.GetAxis("Vertical"); 
         curVelocity = rb.velocity;
 
         //var upPressed = Input.GetKey(KeyCode.UpArrow);
         //var spacePressed = Input.GetKey(KeyCode.Space);
 
-        //Debug.Log($"{upPressed} {spacePressed} {isGrounded()}");
+        //Log($"{upPressed} {spacePressed} {isGrounded()}");
 
         //var mov = new Vector2(0, 0);
 
         //if ((upPressed || spacePressed) && isGrounded())
         //{
-        //    Debug.Log("Jump");
+        //    Log("Jump");
         //    mov.y = jumpSpeed;
         //}
 
         //mov.x = movementSpeed * Input.GetAxis("Horizontal");
 
 
-        //Debug.Log(mov);
+        //Log(mov);
         //rb.AddForce(mov);
     }
 
